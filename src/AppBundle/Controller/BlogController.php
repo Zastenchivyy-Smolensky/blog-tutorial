2a3d1cf91984d38e53e718fa745299a3ec6bea41
Zastenchivyy-Smolensky <s20c2007@bene.fit.ac.jp>
2022-06-21 22:05:49 +0900

    first commit

---
+++ src/AppBundle/Controller/BlogController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Post;
+/**
+ * @Route("/blog")
+ */
+class BlogController extends Controller
+{
+    /**
+     * @Route("/", name="blog_index")
+     */
+    public function indexAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $posts = $em->getRepository(Post::class)->findAll();
+        return $this->render("blog/index.html.twig",[
+            "posts"=>$posts
+        ]);
+    }
+    /**
+     * @Route("/{id}", name="blog_show", requirements={"id"="\d+"})
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $post = $em->getRepository(Post::class)->find($id);
+        if(!$post){
+            throw $this->createNotFoundException("存在しましせん");
+        }
+        return $this->render("blog/show.html.twig", ["post"=>$post]);
+    }
+    /**
+     * @Route("/new", name="blog_new")
+     */
+    public function newAction(Request $request)
+    {
+        $post = new Post();
+        $form = $this->createFormBuilder($post)
+                     ->add("title")
+                     ->add("content")
+                     ->getForm();
+        $form ->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $post->setCreatedAt(new \DateTime());
+            $post->setUpdatedAt(new \DateTime());
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($post);
+            $em->flush();
+            return $this->redirectToRoute("blog_index");
+        }
+         
+        return $this->render("blog/new.html.twig",[
+            "post" => $post,
+            "form" => $form->createView(),
+        ]);
+    }
+    /**
+     * @Route("/{id}/edit", name="blog_edit", requirements={"id"="\d+"})
+     */
+    public function editAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $post = $em->getRepository(Post::class)->find($id);
+        if(!$post){
+            throw $this->createNotFoundException(
+                'No post found for id '.$id
+            );
+        }
+        $form = $this->createFormBuilder($post)
+                     ->add("title")
+                     ->add("content")
+                     ->getForm();
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            //フォームから送信されてきた値と一緒に更新日時も更新して保存
+            $post->setUpdatedAt(new \DateTime());
+            $em->flush();
+            return $this->redirectToRoute("blog_index");
+        }
+        return $this->render("blog/new.html.twig",[
+            //新規作成する時と同じテンプレートを利用
+            "post" => $post,
+            "form" => $form->createView()
+        ]);
+
+    }
+    /**
+     * @Route("/{id}/delete", name="blog_delete", requirements={"id"="\d+"})
+     */
+
+    public function deleteAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $post = $em->getRepository(Post::class)->find($id);
+        if (!$post) {
+            throw $this->createNotFoundException(
+                'No post found for id '.$id
+            );
+        }
+        $em->remove($post);
+        $em->flush();
+        
+        return $this->redirectToRoute("blog_index");
+    }
+}
